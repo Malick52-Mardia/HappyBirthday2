f313a91f6112f73f8ed60da9b6f0358bf8347620
I1-P112 <I1-P112@Pinfo1.local>
2025-08-19 16:58:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/happybirthday2/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.happybirthday2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.happybirthday2.ui.theme.HappyBirthday2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyBirthday2Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color =MaterialTheme.colorScheme.background,
+                ) {
+                    GreetingImage(
+                        message ="Happy Birthday Sam",
+                        from = "Malick",
+
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String,from : String, modifier: Modifier = Modifier) {
+    Column (
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier.padding(8.dp)
+    ){
+
+
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+        )
+        Text(
+            text = from, fontSize = 50.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End),
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String,from : String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty__1_)
+
+        Box {
+            Image(
+                painter = image,
+                contentScale = ContentScale.Crop,
+                alpha = 0.5F,
+                contentDescription = null
+            )
+
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .padding(8.dp)
+
+        )
+        }
+    }
+
+   
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HappyBirthday2Theme {
+        GreetingImage("Happy Birthday Sam", "from Malick")
+    }
+}
